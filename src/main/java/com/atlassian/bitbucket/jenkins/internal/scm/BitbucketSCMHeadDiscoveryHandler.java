f6dac953734af2f234db0beb21cb18dc49198673
Josh Aguilar <jaguilar3@atlassian.com>
2023-05-05 08:46:39 +1200

    Create discovery handler interface

---
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMHeadDiscoveryHandler.java
@@ -0,0 +1,27 @@
+package com.atlassian.bitbucket.jenkins.internal.scm;
+
+import hudson.model.TaskListener;
+import jenkins.scm.api.*;
+
+import java.util.stream.Stream;
+
+/**
+ * Handles the discovery of different head types to be used by the
+ * {@link BitbucketSCMSource#retrieve(SCMSourceCriteria, SCMHeadObserver, SCMHeadEvent, TaskListener)} method as part
+ * or processing a {@link BitbucketSCMSourceRequest request}.
+ */
+public interface BitbucketSCMHeadDiscoveryHandler<H extends SCMHead, R extends SCMRevision> {
+
+    /**
+     * @return as stream of {@link SCMHead heads} to be used for processing the source request
+     */
+    Stream<H> discoverHeads();
+
+    /**
+     * Creates a {@link SCMRevision revision} for the specified head.
+     *
+     * @param head the head to create a revision for
+     * @return the revision for the specified head
+     */
+    R toRevision(H head);
+}
--- src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMSourceContext.java
+++ src/main/java/com/atlassian/bitbucket/jenkins/internal/scm/BitbucketSCMSourceContext.java
@@ -1,6 +1,5 @@
 package com.atlassian.bitbucket.jenkins.internal.scm;
 
-import edu.umd.cs.findbugs.annotations.NonNull;
 import edu.umd.cs.findbugs.annotations.Nullable;
 import hudson.model.TaskListener;
 import jenkins.scm.api.SCMHeadObserver;
@@ -8,15 +7,29 @@ import jenkins.scm.api.SCMSource;
 import jenkins.scm.api.SCMSourceCriteria;
 import jenkins.scm.api.trait.SCMSourceContext;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Objects.requireNonNull;
+
 public class BitbucketSCMSourceContext extends SCMSourceContext<BitbucketSCMSourceContext, BitbucketSCMSourceRequest> {
+
+    private final Map<BitbucketDiscoverableHeadType, BitbucketSCMHeadDiscoveryHandler<?, ?>> discoveryHandlers = new HashMap<>();
 
     public BitbucketSCMSourceContext(@Nullable SCMSourceCriteria criteria, SCMHeadObserver observer) {
         super(criteria, observer);
     }
 
-    @NonNull
     @Override
     public BitbucketSCMSourceRequest newRequest(SCMSource source, @Nullable TaskListener listener) {
         return new BitbucketSCMSourceRequest(source, this, listener);
+    }
+
+    public void withDiscoveryHandler(BitbucketDiscoverableHeadType discoverableHeadType,
+                                     BitbucketSCMHeadDiscoveryHandler<?, ?> discoveryHandler) {
+        requireNonNull(discoverableHeadType, "discoverableHeadType");
+        requireNonNull(discoveryHandler, "discoveryHandler");
+
+        discoveryHandlers.put(discoverableHeadType, discoveryHandler);
     }
 }
